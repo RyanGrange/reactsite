01a655887a9e67640d7e719d0d666c669974f9e3
Ryan Grange <ryangrange@gmail.com>
2024-08-31 18:17:27 -0700

    initialwork: Threw together some basic React elements and API calls.

---
+++ rgreact/src/components/VisitorInfo.tsx
@@ -0,0 +1,14 @@
+import VisitorIp from "./VisitorIp";
+
+function VisitorInfo() {
+  return (
+    <>
+      <div className="greetings">Greetings, visitor.</div>
+      <VisitorIp />
+    </>
+  );
+}
+
+export default VisitorInfo;
+
+//TODO: Ask them to enter their name.
